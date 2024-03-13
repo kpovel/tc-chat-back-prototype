d6cf86b02ab33b08375bebb1e2e33e1916eff6ee
anatolii <bychko4891@gmail.com>
2024-03-13 09:59:05 +0200

    add validation fields in endpoint - create public chat room

--- src/main/java/com/example/chat/controller/ChatRoomController.java
+++ src/main/java/com/example/chat/controller/ChatRoomController.java
@@ -4,10 +4,12 @@ import com.example.chat.model.*;
 import com.example.chat.payload.request.CreatePublicChatRoomRequest;
 import com.example.chat.payload.request.DemoDataPublicChat;
 import com.example.chat.payload.request.EditChatRoomRequest;
+import com.example.chat.payload.response.ParserToResponseFromCustomFieldError;
 import com.example.chat.servise.UserService;
 import com.example.chat.servise.impls.ChatRoomService;
 import com.example.chat.servise.impls.FileService;
 import com.example.chat.servise.impls.HashtagService;
+import com.example.chat.utils.CustomFieldError;
 import com.example.chat.utils.JsonViews;
 import com.example.chat.utils.exception.FileFormatException;
 import com.example.chat.utils.exception.InvalidDataException;
@@ -16,12 +18,20 @@ import com.fasterxml.jackson.annotation.JsonView;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 
 @Tag(name = "Chat room", description = "Rest controller chat room")
@@ -38,6 +48,8 @@ public class ChatRoomController {
     private final UserService userService;
 
     private final FileService fileService;
+
+    private final MessageSource messageSource;
 
 
     @PostMapping("/public-chat-room/create/demo-data")
@@ -69,8 +81,24 @@ public class ChatRoomController {
     @Operation(summary = "Create New public chat room step one", description = "Step one - name, chat type and image file")
     @SecurityRequirement(name = "Bearer Authentication")
     @JsonView(JsonViews.ViewFieldChat.class)
-    public ResponseEntity<ChatRoom> createNewPublicChatRoom(@RequestPart(name = "file", required = false) MultipartFile file,
-                                                            @RequestPart(name = "chatRoom") CreatePublicChatRoomRequest chatRoomRequest) {
+    public ResponseEntity<?> createNewPublicChatRoom( @Valid @RequestPart(name = "chatRoom") CreatePublicChatRoomRequest chatRoomRequest,BindingResult bindingResult,
+                                                      @RequestPart(name = "file", required = false ) MultipartFile file
+
+                                                     ) {
+        Locale currentLocale = LocaleContextHolder.getLocale();
+        if(bindingResult.hasErrors()) {
+            List<CustomFieldError> errorFields = new ArrayList<>();
+            try {
+                errorFields = bindingResult.getFieldErrors().stream()
+                        .map(fieldError -> new CustomFieldError(fieldError.getField(), messageSource.getMessage(fieldError.getDefaultMessage(), null, currentLocale)))
+                        .collect(Collectors.toList());
+                return ResponseEntity.badRequest().body(ParserToResponseFromCustomFieldError.parseCustomFieldErrors(errorFields));
+            } catch (NoSuchMessageException e) {
+                errorFields.clear();
+                errorFields.add(new CustomFieldError("serverError", messageSource.getMessage("server.error", null, currentLocale)));
+                return ResponseEntity.badRequest().body(ParserToResponseFromCustomFieldError.parseCustomFieldErrors(errorFields));
+            }
+        }
         Image image = new Image();
         if(file != null) {
             String contentType = file.getContentType();
--- src/main/java/com/example/chat/payload/request/CreatePublicChatRoomRequest.java
+++ src/main/java/com/example/chat/payload/request/CreatePublicChatRoomRequest.java
@@ -1,5 +1,7 @@
 package com.example.chat.payload.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
@@ -8,7 +10,9 @@ import lombok.Setter;
 @Setter
 public class CreatePublicChatRoomRequest {
 
-    @NonNull
+    @NotNull(message = "field.not.empty")
+    @Size(max = 60, message = "field.max.size")
+    @Size(min = 1, message = "field.min.size")
     private String chatRoomName;
 
     public CreatePublicChatRoomRequest() {
